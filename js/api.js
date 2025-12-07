0d0362cfb6393bc2159f407d5dc620314e9d03a9
NyaLYSM <hglot10@gmail.com>
2025-12-07 11:23:32 +1000

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,13 +1,15 @@
-const API_URL = "https://stylist-backend-h5jl.onrender.com/";
+const API_URL = "https://stylist-backend-h5jl.onrender.com";
 
 export async function getWardrobe() {
-    return fetch(`${API_URL}/wardrobe`).then(res => res.json());
+    const res = await fetch(`${API_URL}/api/wardrobe`);
+    return res.json();
 }
 
 export async function addItem(photoUrl) {
-    return fetch(`${API_URL}/add_item`, {
+    const res = await fetch(`${API_URL}/api/add_item`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ photo_url: photoUrl })
-    }).then(res => res.json());
+    });
+    return res.json();
 }
